6eb9e28d084744ec72dce29375b5a339d9d67916
Elder Gomes <the_elder_12@hotmail.com>
2024-01-12 09:25:50 -0400

    fix getresultados

--- LotteryResult/LotteryResult.Data/Implementations/ProductRepository.cs
+++ LotteryResult/LotteryResult.Data/Implementations/ProductRepository.cs
@@ -4,6 +4,7 @@ using LotteryResult.Data.Models;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,16 +19,18 @@ namespace LotteryResult.Data.Implementations
 
         public async Task<List<Product>> GetResultByProductsByDate(DateTime date)
         {
-            DateTime inicioDelDia = new DateTime(date.Year, date.Month, date.Day, 0, 0, 0);
+            CultureInfo cultureInfo = new CultureInfo("es-VE");
+
+            DateTime inicioDelDia = new DateTime(date.Year, date.Month, date.Day, 0, 0, 0, cultureInfo.Calendar);
             DateTime inicioDelDiaUTC = inicioDelDia.ToUniversalTime();
 
-            DateTime finDelDia = new DateTime(date.Year, date.Month, date.Day, 23, 59, 59);
+            DateTime finDelDia = new DateTime(date.Year, date.Month, date.Day, 23, 59, 59, cultureInfo.Calendar);
             DateTime finDelDiaUTC = finDelDia.ToUniversalTime();
 
             return await _dbContext.Products
                 .Where(x => x.Enable)
-                .Include(x => x.Results.Where(r => r.CreatedAt.ToUniversalTime() >= inicioDelDiaUTC && 
-                                                   r.CreatedAt.ToUniversalTime() <= finDelDiaUTC))
+                .Include(x => x.Results.Where(r => r.CreatedAt >= inicioDelDiaUTC && 
+                                                   r.CreatedAt <= finDelDiaUTC))
                 .ToListAsync();
         }
     }
