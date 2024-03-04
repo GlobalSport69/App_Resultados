6a54e1625be5367bc603631f422e4fb6a9472916
Elder Gomes <the_elder_12@hotmail.com>
2024-03-04 12:47:22 -0400

    agregado endpoint de resultados como api

---
+++ LotteryResult/LotteryResult/Dtos/ResultsDto.cs
@@ -0,0 +1,14 @@
+﻿namespace LotteryResult.Dtos
+{
+    public class ResultsDto
+    {
+        public string ProductName { get; set; }
+        public IEnumerable<ResultDetailDto> Results { get; set; }
+    }
+    public class ResultDetailDto
+    {
+        public string Result { get; set; }
+        public string Time { get; set; }
+        public string? Sorteo { get; set; }
+    }
+}
