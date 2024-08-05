d60cb6ceeddfe25a2155e36d58267df38a0349d6
Elder Gomes <the_elder_12@hotmail.com>
2024-08-05 13:51:26 -0400

    fix

--- LotteryResult/LotteryResult/Services/NotifyPremierService.cs
+++ LotteryResult/LotteryResult/Services/NotifyPremierService.cs
@@ -117,6 +117,8 @@ namespace LotteryResult.Services
                         throw new ArgumentException("PremierHookUrl is empty");
 
                     var result = await hookUrl
+                    .WithHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.84 Safari/537.36")
+                    .WithHeader("Referer", hookUrl)
                     .PostJsonAsync(body)
                     .ReceiveString();
 
